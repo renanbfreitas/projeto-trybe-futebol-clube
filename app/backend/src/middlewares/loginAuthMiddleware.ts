d06ad636b107e088c76d2dc0a1aa3d3208f4988d
Renanbfreitas <renanbf1992@outlook.com>
2023-03-30 12:17:21 -0300

    feat: add req 10

---
+++ app/backend/src/middlewares/loginAuthMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import validateEmail from '../utils/validateEmail';
+
+function loginAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  if (validateEmail(email) === false) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+  if (password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+  next();
+}
+
+export default loginAuthMiddleware;
--- app/backend/src/routes/LoginRoutes.ts
+++ app/backend/src/routes/LoginRoutes.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from 'express';
 import LoginService from '../services/LoginService';
 import LoginController from '../controller/LoginController';
+import loginAuthMiddleware from '../middlewares/loginAuthMiddleware';
 
 const matchesRoutes = Router();
 const loginRouter = new LoginService();
 const loginController = new LoginController(loginRouter);
-matchesRoutes.post('/login', (req: Request, res: Response) => loginController.readOne(req, res));
+matchesRoutes.post(
+  '/login',
+  loginAuthMiddleware,
+  (req: Request, res: Response) => loginController.readOne(req, res),
+);
 
 export default matchesRoutes;
